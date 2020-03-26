2fb51b1c01d16db65ae9320a1f6e2b9ac6e8bf1b
Zach M <zatchmdws@gmail.com>
2020-03-26 15:10:37 -0400

    Made edits to all files, making server function properly

--- models/Workout.js
+++ models/Workout.js
@@ -3,17 +3,56 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    name: {
-        type: String,
-        unique: true
+    day: { 
+        type: Date,
+        default: () => new Date()
     },
-    notes: [
+    exercises: [
         {
-            type: Schema.Types.ObjectId,
-            ref: "Note"
+            type: {
+                type: String,
+                trim: true,
+                required: "A type must be entered"
+            },
+            name: { 
+                type: String,
+                trim: true,
+                required: "A name must be entered"
+            },
+            weight: { 
+                type: Number
+            },
+            sets: { 
+                type: Number
+            },
+            reps: { 
+                type: Number
+            },
+            duration: { 
+                type: Number,
+                required: "An exercise length must be entered"
+            },
+            distance: { 
+                type: Number
+            }
         }
     ]
+},
+
+{
+    toJSON: { 
+        virtuals: true
+    }
+}
+
+);
+
+WorkoutSchema.virtual("totalDuration").get(function() { 
+    return this.exercises.reduce((total, exercise) => { 
+        return total + exercise.duration;
+    }, 0);
 });
+
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
--- server.js
+++ server.js
@@ -15,15 +15,82 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-db.Workout.create({ name: "New Workout"})
+db.Workout.create({ name: ""})
   .then(dbWorkout => { 
     console.log(dbWorkout);
   })
   .catch(({ message }) => { 
     console.log(message);
   });
+
+
+app.post("/", ({ body }, res) => { 
+  db.exercise.create(body)
+  .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id} }, { new: true }))
+  .then(dbWorkout => { 
+    res.json(dbWorkout);
+  })
+  .catch(err => { 
+    res.json(err);
+  })
+})
+
+
+app.get('/exercise', (req, res) => { 
+  res.redirect('exercise.html');
+});
+
+
+app.get('/exercise/:id', (req, res) => { 
+  res.redirect('exercise.html');
+});
+
+
+app.get('/stats', (req, res) => { 
+  res.redirect('stats.html');
+});
+
+
+app.get('/api/workouts', (req, res) => { 
+  db.Workout.find()
+  .then(results => res.json(results))
+});
+
+
+app.post('/api/workouts', (req, res) => { 
+  db.Workout.create({})
+  .then(dbWorkout => { 
+    res.json(dbWorkout);
+  })
+  .catch(err => { 
+    res.json(err);
+  });
+
+})
+
+app.put('/api/workouts/:id', (req, res) => { 
+  db.Workout.findByIdAndUpdate(req.params.id, {$push:{exercises:req.body}},{new: true, runValidators:true})
+  .then(dbWorkout => { 
+    console.log(dbWorkout)
+    res.json(dbWorkout);
+  })
+  .catch(err => { 
+    res.json(err);
+  });
+})
+
+
+app.get('/api/workouts/range', (req, res) => { 
+  db.Workout.find({})
+  .then(dbWorkout => { 
+    res.json(dbWorkout);
+  })
+  .catch(err => { 
+    res.json(err);
+  })
+})
 
 
 
