f458dcc37d2f5c93eee0ad163c089b35b70130a2
Zach M <zatchmdws@gmail.com>
2020-03-26 15:45:57 -0400

    Hopefully making accessible via Heroku!

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+mongoose.connect(MONGODB_URI);
+
 
 db.Workout.create({ name: ""})
   .then(dbWorkout => { 
